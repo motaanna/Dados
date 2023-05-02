41ca17890db07d927a66d51dd5441ee5733942a9
motaanna <annaclaramota70@gmail.com>
2023-05-02 11:01:19 -0300

    dado

--- app/src/main/java/com/example/dados/MainActivity.kt
+++ app/src/main/java/com/example/dados/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.dados
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.dados.ui.theme.DadosTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +35,35 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+       Rolardados()
                 }
             }
         }
     }
 }
-
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DadosTheme {
-        Greeting("Android")
+ fun Rolardados(){
+    var numeroaleatorio by remember { mutableStateOf(1) }
+     Text(
+         text = "Jogar Dados",
+         textAlign = TextAlign.Center,
+         modifier = Modifier.fillMaxSize()
+             .padding(top = 65.dp),
+     )
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+            ){
+        Image(
+            painter = painterResource(id = R.drawable.dice_1) ,
+             contentDescription =null
+        )
+        Button(
+            onClick = { numeroaleatorio =(1..6).random() })
+        {
+            Text(text = "Rolar")
+        }
     }
-}
+ }
